815451450d88f6141c05058b76207a88f7eebe7c
liuzhou755 <liuzhou755@gmail.com>
2024-03-10 21:40:53 +0800

    第一次实验内容（做了一个九宫格图标）

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //面板1的九宫格数组
+    array1:[
+        {icon: '/image/top/money.png',text: '收付款'},
+        {icon: '/image/top/balance.png',text: '零钱\n0.00'},
+        {icon: '/image/top/129 id cards.png',text: '银行卡'},
+    ],
+    //面板2的九宫格数组
+    array2:[
+      {icon: '/image/service/credit-card-repaymen.png',text: '信用卡还款'},
+      {icon: '/image/service/mobile.png',text: '手机充值'},
+      {icon: '/image/service/wealth (1).png',text: '理财通'},
+      {icon: '/image/service/Utilities.png',text: '生活缴费'},
+      {icon: '/image/service/quality.png',text: 'Q币充值'},
+      {icon: '/image/service/public.png',text: '城市服务'},
+      {icon: '/image/service/charity.png',text: '腾讯公益'},
+      {icon: '/image/service/insurance .png',text: '保险服务'},
+      {icon: '/image/service/wealth.png',text: '银行卡'}
+  ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
